d257ae349638d1eee58b378245bc2949a8b94fee
BPitts8019 <bradleyj.pitts@gmail.com>
2019-10-31 19:14:17 -0700

    Added a signout option and conditional rendering of nav

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,30 @@
-import "./App.css"
-import React from "react"
-import {Link, Route} from "react-router-dom";
+import "./App.css";
+import React from "react";
+import {NavLink, Route, withRouter} from "react-router-dom";
+import {getToken} from "../utils/api";
 
 import Signin from "./Signin";
 import Account from "./Account";
 import ProtectedRoute from "./ProtectedRoute";
+import Logout from "./Logout";
 
 function App() {
+   const signin = getToken();
+
 	return (
 		<div className="wrapper">
 			<nav>
-            <Link to="/">Home</Link>
-            <Link to="/signin">Sign In</Link>
-            <Link to="/account">My Account</Link>
+            <NavLink exact to="/">Home</NavLink>
+            {!signin && <NavLink to="/signin">Sign In</NavLink>}
+            {signin && <NavLink to="/account">My Account</NavLink>}
+            {signin && <NavLink to="/logout">Logout</NavLink>}
          </nav>
 
          <Route exact path="/signin" component={Signin} />
          <ProtectedRoute exact path="/account" component={Account} />
+         <ProtectedRoute exact path="/logout" component={Logout} />
 		</div>
 	)
 }
 
-export default App
+export default withRouter(App);
